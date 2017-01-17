262841e6823140bb9bb0aecd3659fa71a84936b4
Salman <2194437M@student.gla.ac.uk@>
2017-01-17 13:42:46 +0000

    Restructuring file directory

--- rango/views.py
+++ rango/views.py
@@ -8,4 +8,4 @@ def index(request):
 	return HttpResponse("Rango says hey there partner! </br> <a href = '/rango/about/'>About</a>")
 
 def about(request):
-	return HttpResponse("Rango says here is the about page </br> <a href = '/rango/'>Back</a>")
+	return HttpResponse("Rango says here is the about page </br> <a href = '/rango/'>Index</a>")
