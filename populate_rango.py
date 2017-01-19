1be4c9107da27ac751a0ae5182400227a8103415
Salman <2194437M@student.gla.ac.uk@>
2017-01-19 20:56:54 +0000

    End of chapter 5 exercises, beginning to debug code usings tests.py

--- populate_rango.py
+++ populate_rango.py
@@ -61,6 +61,15 @@ def add_page(cat, title, url, views=0):
 
 def add_cat(name):
     c = Category.objects.get_or_create(name=name)[0]
+    if c.name == "Python":
+        c.views = 128
+        c.likes = 64
+    elif c.name == "Django":
+        c.views = 64
+        c.likes = 32
+    elif c.name == "Other Frameworks":
+        c.views = 32
+        c.likes = 16
     c.save()
     return c
 
